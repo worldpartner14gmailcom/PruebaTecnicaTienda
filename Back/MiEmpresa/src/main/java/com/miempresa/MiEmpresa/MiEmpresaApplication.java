43aca47a1d0cd8c3eeea36f038ed1151381f3413
Fredy Garcia Lenovo <worldpartner.14@gmail.com>
2023-10-04 11:11:10 -0500

    Cargue inicial del back

---
+++ Back/MiEmpresa/src/main/java/com/miempresa/MiEmpresa/MiEmpresaApplication.java
@@ -0,0 +1,13 @@
+package com.miempresa.MiEmpresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiEmpresaApplication.class, args);
+	}
+
+}
---
+++ Back/MiEmpresa/src/test/java/com/miempresa/MiEmpresa/MiEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miempresa.MiEmpresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiEmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
